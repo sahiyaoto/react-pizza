84ec070aedfa235a01af4116cb007c3abc22df22
sahiyaoto <sahiyaoto@gmail.com>
2020-08-22 11:18:15 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,15 +7,17 @@ function Header() {
   return (
     <div className="header">
       <div className="container">
-        <div className="header__logo">
-          <img width="38" src={logoSvg} alt="Pizza logo" />
-          <div>
-            <h1>React Pizza</h1>
-            <p>самая вкусная пицца во вселенной</p>
+        <Link to="/">
+          <div className="header__logo">
+            <img width="38" src={logoSvg} alt="Pizza logo" />
+            <div>
+              <h1>React Pizza</h1>
+              <p>самая вкусная пицца во вселенной</p>
+            </div>
           </div>
-        </div>
+        </Link>
         <div className="header__cart">
-          <Link to="Cart /">
+          <Link to="/Cart">
             <Button className="button--cart">
               <span>520 ₽</span>
               <div className="button__delimiter"></div>
