31d3238880611a3871e180862476bd9c54b601e8
sahiyaoto <sahiyaoto@gmail.com>
2020-08-22 17:31:56 +0300

    added pizza database

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import { Home, Cart } from './pages';
 import { Route } from 'react-router-dom';
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+  React.useEffect(() => {
+    fetch('http://localhost:3000/db.json').then((resp) =>
+      resp.json().then((json) => {
+        setPizzas(json.pizzas);
+      }),
+    );
+  }, []);
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route path="/" component={Home} exact />
+        <Route path="/" render={() => <Home items={pizzas} />} exact />
         <Route path="/cart" component={Cart} exact />
       </div>
     </div>
