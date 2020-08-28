997c6d2ef46ee16777b2527d9a77ca47b61c226f
sahiyaoto <sahiyaoto@gmail.com>
2020-08-29 00:22:05 +0300

    added Redux-thunk

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import axios from 'axios';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
 import { Route } from 'react-router-dom';
-import { setPizzas } from './redux/actions/pizzas';
+import { fetchPizzas } from './redux/actions/pizzas';
 
 function App() {
   const dispatch = useDispatch();
-
   React.useEffect(() => {
-    axios.get('http://localhost:3001/pizzas').then(({ data }) => {
-      dispatch(setPizzas(data));
-    });
+    dispatch(fetchPizzas());
   }, []);
 
   return (
